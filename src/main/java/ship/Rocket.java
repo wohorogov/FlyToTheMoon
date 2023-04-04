e72db9ede6a35134311643c66f54c7a85d0bf8ce
wohorogov <wohorogov@mail.ru>
2023-04-05 01:28:05 +0500

    Первые классы

--- src/main/java/ship/Rocket.java
+++ src/main/java/ship/Rocket.java
@@ -1,5 +1,11 @@
 package ship;
 
 public class Rocket {
+    public class RocketStage {
+        private double mass;
+        private double speed;
+    }
+    public class RocketBrakeStage {
 
+    }
 }
